ba8c2f2df555127050219be14b69732ed3ee05cf
Andrei Naidenov <andrei.naidenov@dxc.com>
2025-01-22 16:19:10 +0100

    amend solutions template

--- template_main.zig
+++ template_main.zig
@@ -3,5 +3,21 @@ const print = std.debug.print;
 const input = @embedFile("input.txt");
 
 pub fn main() !void {
-    std.debug.print("Hi! {s}\n", .{input});
+    const parsed = parse(input);
+    try part1();
+    try part2();
+}
+
+fn parse(data: []const u8) []const u8 {
+    return data;
+}
+
+fn part1() !void {
+    var result: isize = 0;
+    print("Part1 result: {}\n", .{result});
+}
+
+fn part2() !void {
+    var result: isize = 0;
+    print("Part2 result: {}\n", .{result});
 }
