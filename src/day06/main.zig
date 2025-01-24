924a8b04a8828e1c6e33b0156a6c1f798ce95551
Andrei Naidenov <andrei.naidenov@dxc.com>
2025-01-25 00:30:13 +0100

    add day6 init

---
+++ src/day06/main.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const print = std.debug.print;
+const input = @embedFile("input.txt");
+const dayinfo = @embedFile("dayinfo");
+
+pub fn main() !void {
+    print("Running solution for {s}\n", .{dayinfo});
+    const parsed = parse(input);
+    _ = parsed;
+    try part1();
+    //try part2();
+}
+
+fn parse(data: []const u8) []const u8 {
+    return data;
+}
+
+fn part1() !void {
+    var result: isize = 0;
+    result += 0;
+    print("Part1 result: {}\n", .{result});
+}
+
+fn part2() !void {
+    var result: isize = 0;
+    result += 0;
+    print("Part2 result: {}\n", .{result});
+}
